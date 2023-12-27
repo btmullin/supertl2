ef4bee758b77685107e138d5caf96c633c17362a
Ben Mullin <nerdicconsulting@gmail.com>
2023-12-26 20:48:05 -0600

    Adding an import summary page

--- apps/frontend/views.py
+++ apps/frontend/views.py
@@ -11,7 +11,13 @@ def test():
     return render_template('test.html')
 
 @views.route("/activity")
-def activiy_view():
+def activity_view():
     args = request.args
     activity_id = args.get('id')
     return render_template('activity.html', id=activity_id)
+
+@views.route("/import_summary")
+def import_summary():
+    args = request.args
+    activity_id = args.get('id')
+    return render_template('import_summary.html', id=activity_id)
