84d14991ae6949f49c9754cedd05b42c6e5954c6
Ben Mullin <nerdicconsulting@gmail.com>
2024-01-01 22:38:34 -0600

    generating activity summary

--- apps/common/trainingdata/activity.py
+++ apps/common/trainingdata/activity.py
@@ -9,11 +9,50 @@ import pandas as pd
 @dataclass
 class ActivitySummary:
     title: str = "Untitled Workout"
-    distance: float = 0
-    start_date: date = date.today()
-    start_time: time = datetime.now()
-    active_duration: timedelta = timedelta(0)
-    total_duration: timedelta = timedelta(0)
+    start: datetime = datetime.now()
+    distance_m: float = 0.0
+    active_duration_s: float = 0.0
+    elapsed_duration: float = 0.0
+    elevation_m: float = 0.0
+    avg_speed_mps: float = 0.0
+
+    def __init__(self, source_file: str) -> None:
+        # determine file type
+        extension = os.path.splitext(source_file)[1]
+
+        match extension:
+            case '.fit':
+                # open a fit file
+                self.import_fit(source_file)
+
+            case '.gpx':
+                # open a gpx file
+                self.import_gpx(source_file)
+        
+            case _:
+                # unknown file type
+                # benbug raise error
+                pass
+
+    def import_fit(self, source_file: str) -> None:
+        stream = Stream.from_file(source_file)
+        decoder = Decoder(stream)
+        messages, errors = decoder.read()
+
+        session_data = messages['session_mesgs'][0]
+        self.start = session_data['start_time']
+        self.distance_m = session_data['total_distance']
+        self.active_duration_s = session_data['total_timer_time']
+        self.elapsed_duration_s = session_data['total_elapsed_time']
+        self.elevation_m = session_data['total_ascent']
+        self.avg_speed_mps = session_data['enhanced_avg_speed']
+
+    def import_gpx(self, source_file: str) -> None:
+        # benbug - implement gpx import
+        pass
+
+    def get_active_time(self) -> str:
+        return str(timedelta(seconds=self.active_duration_s)).split('.')[0]
 
 # benbug - where does this belong
 OPTIONAL_FIT_RECORDS = ["enhanced_altitude", "position_lat", "position_long", "enhanced_speed", "fractional_cadence", "heart_rate", "distance"]
@@ -24,8 +63,7 @@ class GPSActivityData:
     # power
     source_file = ''
     activity_hash = 0
-    start_date: date = None
-    start_time: time = None
+    start: datetime = None
     time_series_data: pd.DataFrame = None
 
     def __init__(self, source_file: str) -> None:
@@ -77,6 +115,8 @@ class GPSActivityData:
         self.time_series_data["position_long"] = self.time_series_data["position_long"].apply(GPSUtils.semicircles_to_degrees)
 
         # benbug - parse summary information
+        session_data = messages["session_mesgs"][0]
+        self.start = session_data['start_time']
 
     def import_gpx(self, source_file: str) -> None:
         # benbug - implement gpx import
@@ -91,6 +131,6 @@ class Activity:
     def __init__(self, source_file: str) -> None:
         self.source_file = source_file
         self.gps_data = GPSActivityData(self.source_file)
-        self.summary = ActivitySummary()
+        self.summary = ActivitySummary(self.source_file)
 
         # benbug - get the summary information
