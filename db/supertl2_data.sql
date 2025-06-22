aec5564ab887236c55ed8fd6001dc5a7c04fc876
Ben Mullin <benjamin.t.mullin@gmail.com>
2025-06-21 21:56:53 -0500

    Better default data for supertl.

--- db/supertl2_data.sql
+++ db/supertl2_data.sql
@@ -1,21 +1,24 @@
 -- Initialize Category table 
-INSERT INTO Category(name, parent_id) VALUES('nordic skiing', null);
-INSERT INTO Category(name, parent_id) VALUES('snow', 1);
-INSERT INTO Category(name, parent_id) VALUES('roller', 1);
-INSERT INTO Category(name, parent_id) VALUES('skate', 2);
-INSERT INTO Category(name, parent_id) VALUES('classic', 2);
-INSERT INTO Category(name, parent_id) VALUES('skate', 3);
-INSERT INTO Category(name, parent_id) VALUES('classic', 3);
-INSERT INTO Category(name, parent_id) VALUES('running', null);
-INSERT INTO Category(name, parent_id) VALUES('biking', null);
-INSERT INTO Category(name, parent_id) VALUES('trail', 8);
-INSERT INTO Category(name, parent_id) VALUES('road', 8);
-INSERT INTO Category(name, parent_id) VALUES('road', 9);
-INSERT INTO Category(name, parent_id) VALUES('gravel', 9);
-INSERT INTO Category(name, parent_id) VALUES('mountain', 9);
+INSERT INTO Category(name, parent_id) VALUES('Nordic Skiing', null);
+INSERT INTO Category(name, parent_id) VALUES('Snow', 1);
+INSERT INTO Category(name, parent_id) VALUES('Roller', 1);
+INSERT INTO Category(name, parent_id) VALUES('Skate', 2);
+INSERT INTO Category(name, parent_id) VALUES('Classic', 2);
+INSERT INTO Category(name, parent_id) VALUES('Skate', 3);
+INSERT INTO Category(name, parent_id) VALUES('Classic', 3);
+INSERT INTO Category(name, parent_id) VALUES('Running', null);
+INSERT INTO Category(name, parent_id) VALUES('Biking', null);
+INSERT INTO Category(name, parent_id) VALUES('Trail', 8);
+INSERT INTO Category(name, parent_id) VALUES('Road', 8);
+INSERT INTO Category(name, parent_id) VALUES('Road', 9);
+INSERT INTO Category(name, parent_id) VALUES('Gravel', 9);
+INSERT INTO Category(name, parent_id) VALUES('Mountain', 9);
+INSERT INTO Category(name, parent_id) VALUES('Strength', null);
 
 -- Initialize WorkoutType table
+INSERT INTO WorkoutType(name, description) VALUES('General', 'General endurance workout');
 INSERT INTO WorkoutType(name, description) VALUES('L3', 'Level 3 Intervals');
 INSERT INTO WorkoutType(name, description) VALUES('L4', 'Level 4 Intervals');
 INSERT INTO WorkoutType(name, description) VALUES('OD', 'Over Distance');
 INSERT INTO WorkoutType(name, description) VALUES('Speed', 'Neruomuscular Focused Speeds');
+INSERT INTO WorkoutType(name, description) VALUES('Strength', 'Strength Focused');
