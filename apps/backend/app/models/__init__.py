ebbf42fd38adc990cb2f49d938d4cf0ad256904b
Ben Mullin <benjamin.t.mullin@gmail.com>
2025-11-26 22:10:29 -0600

    Adding additional models for the canonical activity and associated relations

--- apps/backend/app/models/__init__.py
+++ apps/backend/app/models/__init__.py
@@ -3,5 +3,6 @@ from .traininglogdata import TrainingLogData
 from .workouttype import WorkoutType
 from .category import Category
 from .stravaactivitystream import StravaActivityStream
+from .activity import Activity
 
-__all__ = ['StravaActivity', 'TrainingLogData', 'WorkoutType', 'Category','StravaActivityStream']
+__all__ = ['StravaActivity', 'TrainingLogData', 'WorkoutType', 'Category','StravaActivityStream', 'Activity']
